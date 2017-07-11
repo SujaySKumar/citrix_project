79c550ac43b26b207f095d53e5ec434a1af7612c
SujaySKumar <sujay.skumar141295@gmail.com>
2017-07-11 15:08:06 +0530

    UI changes

--- citrix_answers/urls.py
+++ citrix_answers/urls.py
@@ -4,7 +4,7 @@ from django.template import Context
 from . import views
 
 urlpatterns = [
-	url(r'^homepage', views.home, name='home'),
+    url(r'^homepage', views.home, name='home'),
     url(r'^test', views.test_view, name='test_view'),
     url(r'^question/(?P<question_id>[0-9]+)/$', views.question_answer_view, name='question_answer_view'),
     url(r'^add-answer/(?P<question_id>[0-9]+)/$', views.add_answer, name='add_answer'),
